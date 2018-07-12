452a924537e8bc2a24513c07301ddec449fe0844
maliang.huang <maliang.huang@gogen.com.cn>
2018-07-12 10:41:16 +0800

    第二次提交

---
+++ src/main/java/com/gogen/test/test.java
@@ -0,0 +1,24 @@
+package com.gogen.test;
+
+public class test {
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("gogen");
+
+
+
+    }
+}
